6f00da57dd9a053df8998bc2d64c009b93467e6b
Candraak <candraak27@gmail.com>
2021-03-16 13:45:04 +0700

    PBO

---
+++ pertemuan1/teori/persegipanjang.java
@@ -0,0 +1,9 @@
+public class Persegipanjang {
+    public static void main (String args []) {
+        int a = 20, b=20;
+        int luas = a*b*4;
+        System.out.println("Luas 4 Persegi Panjang : "+luas);
+        
+
+    }
+} 
